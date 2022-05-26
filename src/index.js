56ff6ac3604e858a2d91188fe5b080188849a78d
MarcusVMendes <marcus.mendes89@gmail.com>
2022-05-26 17:24:05 -0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -8,11 +8,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={ store }>
+  <Router basename={process.env.PUBLIC_URL}>
+    <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root'),
 );
 
